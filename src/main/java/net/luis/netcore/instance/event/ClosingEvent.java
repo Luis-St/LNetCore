d620aa05af6edcb8462936f1e9649cd323d2f555
Luis <luis@staudt-fridingen.de>
2023-05-27 22:30:09 +0200

    optimized default ClosingEvents

--- src/main/java/net/luis/netcore/instance/event/ClosingEvent.java
+++ src/main/java/net/luis/netcore/instance/event/ClosingEvent.java
@@ -37,34 +37,11 @@ public interface ClosingEvent<E extends Event> {
 	}
 	
 	static @NotNull ClosingEvent<SendEvent> closeAfterSent(Class<? extends Packet> packet) {
-		return new ClosingEvent<SendEvent>() {
-			@Override
-			public @NotNull EventType<SendEvent> getEvent() {
-				return SEND;
-			}
-			
-			@Override
-			public boolean shouldClose(SendEvent event) {
-				return packet.isAssignableFrom(event.getPacket().getClass());
-			}
-		};
+		return closeAfterSent(packet, 1);
 	}
 	
 	static @NotNull ClosingEvent<SendEvent> closeAfterSent(int packets) {
-		return new ClosingEvent<SendEvent>() {
-			private final Map<UUID, Integer> hits = Maps.newHashMap();
-			
-			@Override
-			public @NotNull EventType<SendEvent> getEvent() {
-				return SEND;
-			}
-			
-			@Override
-			public boolean shouldClose(SendEvent event) {
-				this.hits.compute(event.getUniqueId(), (uuid, integer) -> integer == null ? 1 : integer + 1);
-				return this.hits.getOrDefault(event.getUniqueId(), 0) >= packets;
-			}
-		};
+		return closeAfterSent(Packet.class, packets);
 	}
 	
 	static @NotNull ClosingEvent<SendEvent> closeAfterSent(Class<? extends Packet> packet, int packets) {
@@ -104,34 +81,11 @@ public interface ClosingEvent<E extends Event> {
 	}
 	
 	static @NotNull ClosingEvent<ReceiveEvent> closeAfterReceived(Class<? extends Packet> packet) {
-		return new ClosingEvent<ReceiveEvent>() {
-			@Override
-			public @NotNull EventType<ReceiveEvent> getEvent() {
-				return RECEIVE;
-			}
-			
-			@Override
-			public boolean shouldClose(ReceiveEvent event) {
-				return packet.isAssignableFrom(event.getPacket().getClass());
-			}
-		};
+		return closeAfterReceived(packet, 1);
 	}
 	
 	static @NotNull ClosingEvent<ReceiveEvent> closeAfterReceived(int packets) {
-		return new ClosingEvent<ReceiveEvent>() {
-			private final Map<UUID, Integer> hits = Maps.newHashMap();
-			
-			@Override
-			public @NotNull EventType<ReceiveEvent> getEvent() {
-				return RECEIVE;
-			}
-			
-			@Override
-			public boolean shouldClose(ReceiveEvent event) {
-				this.hits.compute(event.getUniqueId(), (uuid, integer) -> integer == null ? 1 : integer + 1);
-				return this.hits.getOrDefault(event.getUniqueId(), 0) >= packets;
-			}
-		};
+		return closeAfterReceived(Packet.class, packets);
 	}
 	
 	static @NotNull ClosingEvent<ReceiveEvent> closeAfterReceived(Class<? extends Packet> packet, int packets) {
